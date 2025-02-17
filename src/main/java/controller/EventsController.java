dd50471157c4a6cb792c87c16c6d6ab8cf3126de
Selim <selim.hassani@esprit.tn>
2025-02-17 09:54:27 +0100

    working database display in javafx

--- src/main/java/controller/EventsController.java
+++ src/main/java/controller/EventsController.java
@@ -1,6 +1,23 @@
 package controller;
 
+import javafx.fxml.FXML;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import tn.esprit.services.ServiceEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class EventsController {
+
+    @FXML
+    private TextField tfName;
+    @FXML
+    private Label labelDisplay;
+    ServiceEvent sp = new ServiceEvent();
+
+    @FXML
+    public void displayEvents(javafx.event.ActionEvent actionEvent) {
+        labelDisplay.setText(sp.getAll().toString());
+    }
 
 }
