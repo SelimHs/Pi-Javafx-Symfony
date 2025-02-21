8aec215578f21f4191d1a9bd71a33ae3feb8c1e9
yahya <yahia.karoui@esprit.tn>
2025-02-21 11:38:19 +0100

    integration complet 100%

--- src/main/java/controller/ModifierEventController.java
+++ src/main/java/controller/ModifierEventController.java
@@ -61,6 +61,58 @@ public class ModifierEventController {
             return;
         }
 
+        if (nomEventField.getText() == null || nomEventField.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Le champ 'Nom de l'événement' ne peut pas être vide.").showAndWait();
+            return;
+        }
+
+
+        String selectedDate = dateField.getText().toString();
+        if (selectedDate == null) {
+            new Alert(Alert.AlertType.ERROR, "Le champ 'Date de l'événement' ne peut pas être vide.").showAndWait();
+            return;
+        }
+
+        if (prixField == null || prixField.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Le champ 'Prix' ne peut pas être vide.").showAndWait();
+            return;
+        }
+
+        try {
+            int prix = Integer.parseInt(prixField.getText());
+        } catch (NumberFormatException e) {
+            new Alert(Alert.AlertType.ERROR, "Le champ 'Prix' doit être un entier valide.").showAndWait();
+            return;
+        }
+
+
+
+        if (nbrVisiteursField.getText() == null || nbrVisiteursField.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Le champ 'Nombre de visiteurs' ne peut pas être vide.").showAndWait();
+            return;
+        }
+
+
+
+        if (detailsField.getText() == null || detailsField.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Le champ 'Détails' ne peut pas être vide.").showAndWait();
+            return;
+        }
+
+
+        if (nomEspaceField.getText() == null || nomEspaceField.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Le champ 'Espace' ne peut pas être vide.").showAndWait();
+            return;
+        }
+
+        try {
+            int nbrVisiteurs = Integer.parseInt(nbrVisiteursField.getText());
+        } catch (NumberFormatException e) {
+            new Alert(Alert.AlertType.ERROR, "Le champ 'Nombre de visiteurs' doit être un entier valide.").showAndWait();
+            return;
+        }
+
+
 
         newEvent.setNomEvent(nomEventField.getText());
         newEvent.setPrix(Integer.parseInt(prixField.getText()));
