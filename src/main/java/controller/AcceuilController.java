6b8ae01d2fc4557430e67e4728e4a03e98f453ab
yahya <yahia.karoui@esprit.tn>
2025-03-06 13:18:43 +0100

    correction de l'affichage dans backend

--- src/main/java/controller/AcceuilController.java
+++ src/main/java/controller/AcceuilController.java
@@ -88,7 +88,7 @@ public class AcceuilController {
     }
     public void goToReservation(ActionEvent actionEvent) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GestionReservation.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/AfficheReservation.fxml"));
             Parent root = fxmlLoader.load();
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 
@@ -102,7 +102,7 @@ public class AcceuilController {
 
     public void goToRemise(ActionEvent actionEvent) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GestionRemise.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/AfficherRemise.fxml"));
             Parent root = fxmlLoader.load();
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 
@@ -141,7 +141,7 @@ public class AcceuilController {
 
     public void goToEspace(ActionEvent actionEvent) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GestionEspace.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/AfficherEspaces.fxml"));
             Parent root = fxmlLoader.load();
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 
